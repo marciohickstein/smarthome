1595f52de5f47af116e8542196ce0d844aa8f801
Marcio <marcio.hickstein@gmail.com>
2021-05-27 16:33:30 -0300

    Ajustes para ler dados dos dispositivos de sensores antes de retornar lista dos dispositivos para cliente

--- routes/devices.js
+++ routes/devices.js
@@ -1,11 +1,13 @@
 const DEFAULT_PIN = 17;
 const DEFAULT_PULSELENGTH = 350;
 const DEFAULT_PROTOCOL = 1;
+const TYPE_DHT_SENSOR = 4;
 
 const router = require('express').Router();
+const rpi433 = require('rpi-433-v3');
+const sensor = require('node-dht-sensor').promises;
 const { ContextDataSource } = require('../utils/db/strategies/base/contextDataSource');
 const { JSONDataSource } = require('../utils/db/jsonDataSource');
-const rpi433 = require('rpi-433-v3');
 
 const rfEmitter = rpi433.emitter({
 	pin: DEFAULT_PIN,
@@ -16,13 +18,49 @@ const rfEmitter = rpi433.emitter({
 const dataSource = new ContextDataSource(new JSONDataSource());
 dataSource.open('../../data/devices.json');
 
+sensor.initialize({
+	test: {
+		fake: {
+			temperature: 30,
+			humidity: 60,
+		},
+	},
+});
+
+const checkSensors = async (devices) => {
+	const devicesChanged = devices.map(async (device) => {
+		if (device.type === TYPE_DHT_SENSOR) {
+			try {
+				const res = await sensor.read(22, 4);
+				await dataSource.update(device.id, { value: res.temperature });
+				// eslint-disable-next-line no-param-reassign
+				device.value = res.temperature;
+				console.log(`temp: ${device.value.toFixed(1)}°C, humidity: ${device.value.toFixed(1)}%`);
+			} catch (err) {
+				console.error('Failed to read sensor data:', err);
+			}
+		}
+
+		const deviceChanged = { ...device };
+		console.log(deviceChanged);
+		return deviceChanged;
+	});
+
+	return devicesChanged;
+};
+
 router.get('/', async (req, res) => {
 	const devices = await dataSource.select({});
-	return res.json(devices);
+	const devicesChanged = await checkSensors(devices);
+	console.log(devicesChanged);
+	return res.json(devicesChanged);
 });
 
 router.get('/:id', async (req, res) => {
 	const devicesFiltered = await dataSource.selectById(req.params.id);
+	if (devicesFiltered.type === TYPE_DHT_SENSOR) {
+		checkSensors([...devicesFiltered]);
+	}
 	return res.json(devicesFiltered);
 });
 
